8964a181e59fdf065457c85f1a08b6c599676246
lenghuo01 <1946949433@qq.com>
2023-07-28 10:34:38 +0800

    :

--- jichu/this.js
+++ jichu/this.js
@@ -37,3 +37,44 @@ console.log(obj3.name)
 
 // 请仿写数组的yourselfForeach方法
 const arr=[]
+
+
+// 改变this指向
+//1.call
+//2.aply
+//3.bind
+
+const test={
+    name:'test',
+    aaa:function(time){
+        console.log(time+this)
+    }
+}
+const people={
+    name:'people'
+}
+const test2 =test.aaa
+//apply
+// people.test2('30:31')
+test2.apply(people,['30:31'])
+
+//call
+function test3(){
+//内置属性
+console.log(arguments)          //function(item){console.log(item)}
+Array.prototype.forEach.call(arguments,(item)=>console.log(item))
+}
+
+test3(1,2,3)
+
+//bind
+const free=test.aaa
+const bindfree=free.bind(test)
+bindfree(100)
+
+
+//箭头函数和普通函数的区别
+// 箭头函数没有this，但是可以访问外层function类型函数的this
+// 没有arguments
+//箭头函数不能作为构造函数
+//箭头函数没有prototype
